72dd1390c44ec00b327b78d9ead9909a0d7fa86e
hungDPM245424 <hung@student.agu.edu.vn>
2025-09-18 09:27:21 +0700

    update bai nop buoi dau tien

---
+++ chuong2_cau4.py
@@ -0,0 +1,5 @@
+'''
+Câu hỏi: python hỗ trợn hững kiểu dữ liệu cơ bản nào? 
+Trả lời:
+bao gồm: int, float, complex, str, list, tuple, range, dict, set, frozenset, bool, byte, bytearrray, memoryview
+'''
